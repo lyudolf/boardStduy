c7414b70cefdee54047885cf3a5413b3c130dadb
iplay3473@gmail.com <iplay3473@gmail.com>
2022-08-11 23:39:09 +0900

    게시글 삭제

--- board/src/main/java/com/example/demo/controller/BoardController.java
+++ board/src/main/java/com/example/demo/controller/BoardController.java
@@ -40,5 +40,11 @@ public class BoardController {
 		
 		return "boardview";
 	}
+	@GetMapping("/board/delete")
+	public String boardDelete(Integer id) {
+		boardService.boardDelete(id);
+		
+		return "redirect:/board/list";
+	}
 
 }
